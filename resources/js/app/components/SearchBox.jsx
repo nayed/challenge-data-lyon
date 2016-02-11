e2bb8af347f521d4781e564afc5fa35888722615
roro71680 <romane.mosson@gmail.com>
2016-02-11 10:03:05 +0100

    Recherche ville

--- resources/js/app/components/SearchBox.jsx
+++ resources/js/app/components/SearchBox.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import $ from 'jquery'
 
+
+
+
+
 export default class SearchBox extends React.Component {
     searchMarket() {
         let markets = this.props.search
@@ -20,13 +24,10 @@ export default class SearchBox extends React.Component {
     }
 
     render() {
-        //console.log(this.props.search)
+
         return (
             <div className="app__wrapper">
-                <section className="map-container">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d89077.13001931181!2d4.835120949999999!3d45.7579555!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sfr!2sfr!4v1455092172025" width="100%" height="100%" frameBorder="0" style={{border:0}} allowFullScreen>
-                    </iframe>
-                </section>
+                <div id="map"></div>
                 <section className="header">
                     <div className="header__logo">
                         <i className="fa fa-shopping-basket"></i>
@@ -36,7 +37,7 @@ export default class SearchBox extends React.Component {
                 <section className="search">
                     <div className="search__wrapper">
                         <div className="search__bar">
-                            <input type="text" className="search__input" placeholder="Recherche à proximité de ..."></input>
+                            <input type="text" className="search__input" placeholder="Recherche à proximité de ..." onkeyup="RecupChangementVille(this.value)"></input>
                             <button className="search__button search__button--close"><i className="fa fa-times"></i>
                             </button>
                         </div>
@@ -44,29 +45,40 @@ export default class SearchBox extends React.Component {
                             <li className="search__item search__item--prox"><i className="fa fa-dot-circle-o"></i>
                                 Ma position actuelle
                             </li>
-                            <li className="search__item">Lyon</li>
+                            <li className="search__item" data-checked="true">Lyon</li>
                             <li className="search__item">Chassieu</li>
                             <li className="search__item">Villeurbanne</li>
                             <li className="search__item">Mâcon</li>
-                            <li className="search__item">vaux-en-velin</li>
+                            <li className="search__item">Vaulx-en-Velin</li>
                         </ul>
                     </div>
                 </section>
                 <section className="days">
                     <ul className="days__list">
-                        <li className="days__item" data-value="lundi" data-checked="true">Lun</li>
-                        <li className="days__item" data-value="mardi" data-checked="false">Mar</li>
-                        <li className="days__item" data-value="mercredi" data-checked="true">Mer</li>
-                        <li className="days__item" data-value="jeudi" data-checked="false">Jeu</li>
-                        <li className="days__item" data-value="vendredi" ata-checked="false">Ven</li>
-                        <li className="days__item" data-value="samedi" data-checked="false">Sam</li>
-                        <li className="days__item" data-value="dimanche" data-checked="true">Dim</li>
+                        <li className="days__item" data-value="lundi" id="jour_1" value="1" data-checked="false" onClick={this.test()}>Lun</li>
+                        <li className="days__item" data-value="mardi" id="jour_2" value="2" data-checked="false">Mar</li>
+                        <li className="days__item" data-value="mercredi" id="jour_3" value="3" data-checked="false">Mer</li>
+                        <li className="days__item" data-value="jeudi" id="jour_4" value="4" data-checked="false">Jeu</li>
+                        <li className="days__item" data-value="vendredi" id="jour_5" value="5" data-checked="false">Ven</li>
+                        <li className="days__item" data-value="samedi" id="jour_6" value="6" data-checked="false">Sam</li>
+                        <li className="days__item" data-value="dimanche" id="jour_7" value="7" data-checked="false">Dim</li>
                     </ul>
                 </section>
+                <div id="malongitude"></div>
+                <div id="malatitude"></div>
+
                 <script>
 
                 </script>
+
             </div>
-        )
+        )       
     }
-}
+
+    test(){
+
+        console.log("test");
+
+    }
+
+  }
